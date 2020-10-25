4727e42d0ffa3eaa5af0e120bb8bef0dda0f694c
ysrael-aime-ndri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2020-10-25 07:16:46 +0000

    geturls

--- bin/controllers/render/geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee.php
+++ bin/controllers/render/geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee.php
@@ -45,7 +45,7 @@ class geturlspath
     /* 
         Lancer l'application 
     */
-    public function runApp()
+    public function runApp44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee()
     {
         $this->url = $this->geturi();
 
--- index.php
+++ index.php
@@ -7,6 +7,6 @@ require 'bin/epaphrodite/define/define_directory.php';
 require _DIR_VENDOR_ . '/autoload.php';
 require _DIR_VENDOR_ . '/Autoloader.php';
 
-$path_url = new \bin\controllers\render\geturlspath;
-$path_url->runApp();
+$path_url = new \bin\controllers\render\geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee;
+$path_url->runApp44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee();
 
