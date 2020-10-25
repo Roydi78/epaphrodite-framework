19656b3f45a6222f428be4131dc419d84176c2be
ysrael-aime-ndri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2020-10-25 07:17:22 +0000

    Update geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee.php

--- bin/controllers/render/geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee.php
+++ bin/controllers/render/geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee.php
@@ -3,7 +3,7 @@
 namespace bin\controllers\render;
 
 
-class geturlspath
+class geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee
 {
     private $path_session;
     private $startsession;
