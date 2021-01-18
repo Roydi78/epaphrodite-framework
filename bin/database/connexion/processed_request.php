754fc869bfd94c7bc3489b3de57879a84798cd49
ysrael-aime-ndri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2021-01-18 13:11:13 +0000

    update -commit

--- bin/database/connexion/processed_request.php
+++ bin/database/connexion/processed_request.php
@@ -6,52 +6,70 @@ use bin\database\connexion\database;
 class processed_request
 {
 
-    /* 
-      Get database connexion class
-    */    
-    function __construct()
-    {
+
+    /**
+     * Processed all select request
+     *
+     * @param string $sql
+     * @param string|null $Parametre
+     * @param array|null $datasparam
+     * @param boolean|null $etat
+     * @return void
+     */
+    public function select_request( string $sql, ?string $Parametre=null , ?array $datasparam = null , ?bool $etat=false ){
       $this->connexion = new database();
-    }
-
-    /* 
-      Processed all select request
-    */    
-    public function select_request( $sql, $Parametre , $datasparam = array() , $etat ){
-
         $result = $this->connexion->select($sql, $Parametre, $datasparam);
-        if($etat===true){ $this->connexion->closeConnection(); }
+        if($etat===true){
+          $this->connexion->closeConnection(); }
         return $result;
 
     }
 
-    /* 
-      Processed all update request
-    */    
-    public function update_request($sql, $Parametre, $datasparam = array() , $etat ){
-
+    /**
+     * Processed all update request
+     *
+     * @param string $sql
+     * @param string|null $Parametre
+     * @param array|null $datasparam
+     * @param boolean|null $etat
+     * @return void
+    */  
+    public function update_request( string $sql , ?string $Parametre=null , ?array $datasparam = null , ?bool $etat = false ){
+      $this->connexion = new database();
         $result = $this->connexion->update($sql, $Parametre, $datasparam);
         if($etat===true){ $this->connexion->closeConnection(); }
         return $result;
 
     }  
     
-    /* 
-      Processed all insert request
-    */    
-    public function insert_request($sql, $Parametre, $datasparam = array(), $etat ){
-
+    /**
+     * Processed all insert request
+     *
+     * @param string $sql
+     * @param string|null $Parametre
+     * @param array|null $datasparam
+     * @param boolean|null $etat
+     * @return void
+     */   
+    public function insert_request( string $sql , ?string $Parametre = null , ?array $datasparam = null , ?bool $etat = false ){
+      $this->connexion = new database();
         $result = $this->connexion->insert($sql, $Parametre, $datasparam);
         if($etat===true){ $this->connexion->closeConnection(); }
         return $result;
 
     }    
     
-    /* 
-      Processed all delete request
-    */    
-    public function delete_request($sql, $Parametre, $datasparam = array() , $etat ){
-
+    /**
+     * Processed all delete request
+     *
+     * @param string $sql
+     * @param string|null $Parametre
+     * @param array|null $datasparam
+     * @param boolean|null $etat
+     * @return void
+     */    
+    public function delete_request( string $sql , ?string $Parametre = null , ?array $datasparam = null , ?bool $etat = false ){
+      $this->connexion = new database();
         $result = $this->connexion->delete($sql, $Parametre, $datasparam);
         if($etat===true){ $this->connexion->closeConnection(); }
         return $result;
