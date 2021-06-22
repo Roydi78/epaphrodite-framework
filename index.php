031b20ed10e992e1bf967060f212d5a6d1626ca6
Y'srael Aime N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2021-06-22 11:32:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-var_dump(hash('gost','000749@dsps@000749'));die();
+//var_dump(hash('gost','000749@dsps@000749'));die();
 
 require 'bin/epaphrodite/define/define_directory.php';
 
@@ -9,4 +9,3 @@ require _DIR_VENDOR_ . '/Autoloader.php';
 
 $path_url = new \bin\controllers\render\geturlspath44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee;
 $path_url->runApp44d6fb7ae7c38f949af7f9140d3dc97c26da23c464341310c11bb2f7f3b234ee();
-
